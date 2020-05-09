0850e4a2c77b8c6425c019f6f7def26722a87d93
DESKTOP-D7QS93T\pdavala <pdavala@us.ibm.com>
2020-05-09 11:17:48 -0400

    Testing branch called feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hara hara mahadeva <code>src/App.js</code> and save to reload.
+         Hara hara mahadeva !!! Changed on feature branch -- <code>src/App.js</code>
         </p>
         <a
           className="App-link"
